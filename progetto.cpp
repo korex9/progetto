5cfa6e90d49d505bda1bf661beb992e0c217a25a
RicPiov <riccardo.piovesan.2008@gmail.com>
2025-04-12 10:30:48 +0200

    nando

--- progetto.cpp
+++ progetto.cpp
@@ -3,7 +3,7 @@
 #include <cstdlib>
 #include <ctime>
 
-void nando();
+
 using namespace std;
 
 int main(){
@@ -11,7 +11,4 @@ int main(){
 
     cout<<"Piovesan basta sei un heigui"<<endl;
     return 0;
-}
-void nando(){
-    cout<<"NAAAAAAAAANDO"<<endl;
 }
