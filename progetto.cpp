2439ac12afd4e3883c06ec2d7360db264123bf48
RicPiov <riccardo.piovesan.2008@gmail.com>
2025-04-12 11:37:40 +0200

    funzione con entrata\uscita

--- progetto.cpp
+++ progetto.cpp
@@ -18,25 +18,48 @@ void richiestaProdotto();
 
 int main(){
 
+    richiestaProdotto ();
+
     return 0;
 }
 
-void richiestaProdotto(Spesa spesa){
+void richiestaProdotto(){
+    Spesa spesa;
     string nomeFile = "spese.txt";
     ofstream file(nomeFile);
+    char s;
 
     if (!file) {
         cout<< "Errore nell'apertura del file." << endl;
         return;
     }
 
-    cout << "Inserisci la descrizione della spesa: ";
+    cout << "Inserisci la categoria della spesa: ";
     cin >> spesa.categoria;
 
     cout << "Inserisci il prezzo della spesa: ";
     cin >> spesa.prezzo;
 
-    file << spesa.categoria << " " << spesa.prezzo << endl;
+    do{
+        cout << "Inserisci 'U' se la spesa e' un'uscita oppure 'E' se è un'entrata: ";
+        cin >> s;
+        if (s!='U' && s!='E')
+            cout << "Inserimento errato...riprova!"<<endl;
+    }while (s!='U' && s!='E');
+
+    if(s=='U'){
+        spesa.uscita=true;
+        spesa.entrata=false;
+    }
+    else{
+        spesa.uscita=false;
+        spesa.entrata=true;
+    }
+
+    if(spesa.uscita)
+        file <<"Categoria: "<<spesa.categoria << "; prezzo: " << spesa.prezzo << "; uscita." << endl;
+    else if(spesa.entrata)
+        file <<"Categoria: "<<spesa.categoria << "; prezzo: " << spesa.prezzo << "; entrata." << endl;
 
     file.close();
 }
