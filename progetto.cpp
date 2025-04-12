640193e9fc1e5522b3321ddac716da4e246d1f81
korex9 <faggifabio9@gmail.com>
2025-04-12 10:54:50 +0200

    readfile

--- progetto.cpp
+++ progetto.cpp
@@ -12,12 +12,30 @@ struct Spesa
 };
 
 
-void richiestaProdotto(){
-
-}
-
+void richiestaProdotto();
 
 int main(){
 
     return 0;
+}
+
+void richiestaProdotto(){
+    Spesa spesa;
+    string nomeFile = "spese.txt";
+    ofstream file(nomeFile, ios::app);
+
+    if (!file) {
+        cerr << "Errore nell'apertura del file." << endl;
+        return;
+    }
+
+    cout << "Inserisci la descrizione della spesa: ";
+    cin >> spesa.descrizione;
+
+    cout << "Inserisci il prezzo della spesa: ";
+    cin >> spesa.prezzo;
+
+    file << spesa.descrizione << " " << spesa.prezzo << endl;
+
+    file.close();
 }
