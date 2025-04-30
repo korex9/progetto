4a2db1e63ba563019ffb2acec64b0b851be876e8
RicPiov <riccardo.piovesan.2008@gmail.com>
2025-04-30 14:51:48 +0200

    Aggiunta funzione stampaEU

--- progetto.cpp
+++ progetto.cpp
@@ -11,30 +11,26 @@ struct Spesa
     bool uscita;
 };
 
-void inserimentoProdoto();
-void menu (int s);
-void fSwitch (int s);
-void stampaEU ();
-
-void richiestaProdotto(string oldFile);
+void menu (int &s); //menù per le scelte
+void fSwitch (int s, string oldFile, Spesa &spesa); //funzione con lo switch
+void richiestaProdotto(string oldFile, Spesa &spesa);
+void stampaEU (Spesa &spesa);
 void salvaFile(string nomeFile, string oldFile);
 
 int main(){
+    Spesa spesa;
     int scelta=0;
+    string oldFile = "speseOld.txt";
 
     do{
         menu (scelta);
-        fSwitch(scelta);
+        fSwitch(scelta, oldFile, spesa);
     }while(scelta!=0);
-
-    string oldFile = "speseOld.txt";
-    richiestaProdotto(oldFile);
 
     return 0;
 }
 
-void richiestaProdotto(string oldFile){
-    Spesa spesa;
+void richiestaProdotto(string oldFile, Spesa &spesa){
     string nomeFile = "spese.txt";
     ofstream file(nomeFile);
     char s;
@@ -44,6 +40,7 @@ void richiestaProdotto(string oldFile){
         return;
     }
 
+    cin.ignore();
     cout << "Inserisci la categoria della spesa: ";
     getline(cin, spesa.categoria);
 
@@ -76,9 +73,9 @@ void richiestaProdotto(string oldFile){
     file.close();
 }
 
-void menu (int s){
+void menu (int &s){
 
-    cout<<"Inserire un numero per effetuare un'operazione: "<<endl;
+    cout<<"-----GESTORE DI SPESE PERSONALI-----"<<endl;
     cout<<"1- Inserimento di un prodotto."<<endl;
     cout<<"2- Stampa delle entrate e delle uscite mensili."<<endl;
     cout<<"3- Stampa differenza tra entrate e uscite mensili."<<endl;
@@ -86,30 +83,32 @@ void menu (int s){
     cout<<"5- Stampa differenza tra entrate e uscite mensili di una categoria."<<endl;
     cout<<"6- Stampa della percentuale delle entrate e uscite di una categoria."<<endl;
     cout<<"0- Esci."<<endl;
+    cout<<"Inserire un numero per effetuare un'operazione: ";
     cin>>s;
+    cout<<endl;
 }
 
-void fSwitch (int s){
+void fSwitch (int s, string oldFile, Spesa &spesa){
 
     switch (s)
     {
     case 1:
-        inserimentoProdoto ();
+        richiestaProdotto(oldFile, spesa);
         break;
     case 2:
-        inserimentoProdoto ();
+        stampaEU (spesa);
         break;
     case 3:
-        inserimentoProdoto ();
+        
         break;
     case 4:
-        inserimentoProdoto ();
+        
         break;
     case 5:
-        inserimentoProdoto ();
+       
         break;
     case 6:
-        inserimentoProdoto ();
+        
         break;
     
     default:
@@ -121,6 +120,8 @@ void salvaFile(string nomeFile, string oldFile) {
     ifstream file(nomeFile);
     ofstream oldFileStream(oldFile);
     string line, oldLine;
+
+    cout<<endl;
 
     if (!file || !oldFileStream) {
         cout<< "Errore nell'apertura del file." << endl;
@@ -136,4 +137,23 @@ void salvaFile(string nomeFile, string oldFile) {
     oldFileStream.close();
 
     cout << "File salvato correttamente in " << oldFile << endl;
+}
+
+void stampaEU (Spesa &spesa){
+
+    cout<<"Il totale delle entrate ammonta a: "<<endl;
+    if(spesa.entrata){
+        cout<<spesa.prezzo;
+    }
+    cout<<" euro."<<endl;
+
+    cout<<"Il totale delle uscite ammonta a: "<<endl;
+    if(spesa.uscita){
+        cout<<spesa.prezzo;
+    }
+    cout<<" euro."<<endl;
+
+    cout<<endl;
+    
+
 }
